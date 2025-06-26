5e4d877d21a6b15ec7ca78e64a879dee6b1d4a8a
TarunZach <tzacharias.97@gmail.com>
2025-06-26 14:10:19 +0200

    update: main temp

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,16 @@ int main(int argc, char **argv)
         std::cerr << "Usage: " << argv[0] << " <input.mp4> <output.mp4>\n";
         return -1;
     }
+
     std::string inPath = argv[1], outPath = argv[2];
     VideoReader reader(inPath);
     OpenCLDriver processor;
-    Encoder encoder(outPath);
+
+    int width = reader.getWidth();
+    int height = reader.getHeight();
+    double fps = reader.getFPS();
+
+    Encoder encoder(outPath, width, height, fps);
 
     cv::Mat frame;
     std::vector<uint8_t> yuv;
@@ -22,5 +28,7 @@ int main(int argc, char **argv)
         processor.processFrame(frame, yuv);
         encoder.encodeFrame(yuv);
     }
+
+    encoder.finish();
     return 0;
 }
